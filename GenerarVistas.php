da61b2003019d2153346bb1e3ff0a77bdaac6b98
Adrián <asfarinhas@esei.uvigo.es>
2017-04-20 14:26:10 +0400

    Update GenerarVistas.php
    añadidos metodos crear()

--- GenerarVistas.php
+++ GenerarVistas.php
@@ -1,37 +1,70 @@
 <?php
-    echo "Iniciando creacción de vistas...";
+    echo "Iniciando creacción de vistas...";//Mostramos mensaje para saber cuando empieza
 
-function conectarBD(){
 
+function conectarBD(){//Creamos una funcion para conectarnos a la BD
     $bd = new mysqli("localhost", "root", "iu", "MOOVETT");
     if (mysqli_connect_errno()){
         echo "Fallo al conectar MySQL: " . $this->mysqli->connect_error();
     }
     return $bd;
 }
-function listarTablas()
+function listarTablas() //Creamos una funcion para que nos devuelva todas las tablas de la BD
 {
     $this->mysqli = conectarBD();
-
-    $sql = 'Sshow full tables from MOOVETT';
+    $sql = 'show full tables from MOOVETT';
     if (!($resultado = $this->mysqli->query($sql))) {
         return 'Error en la consulta sobre la base de datos';
     } else {
-        $result = $resultado->fetch_array();
+        $result = $resultado->fetch_array(); //Convertimos el resultado de la consulta a un array asociativo
         return $result;
     }
 }
-$arrayTablas = listarTablas();
-foreach($arrayTablas as $vista){
-    crearVistas($vista);
+
+$arrayTablas = listarTablas();//Llamamos a la funcion listarTablas() para que nos devuelva todas las tablas. Le llamamos $arrayTablas
+foreach($arrayTablas as $tabla){//Recorremos el array con las vistas
+    crearVistas($tabla);//Llamamos a la funcion crear vistas
+    crearADD($tabla);
+    crearSEARCH($tabla);
+    crearEDIT($tabla);
+    crearDELETE($tabla);
+    crearSHOWALL($tabla);
+    crearSHOWCURRENT($tabla);
+
 }
 
-function crearVistas($vista){
-    fopen("../Views/" . strtoupper($vista). "_ADD_View.php","w+");
-    fopen("../Views/" . strtoupper($vista)_ "SEARCH_View.php","w+");
-    fopen("../Views/" . strtoupper($vista)_ "EDIT_View.php","w+");
-    fopen("../Views/" . strtoupper($vista)_ "DELETE_View.php","w+");
-    fopen("../Views/" . strtoupper($vista)_ "SHOWALL_View.php","w+");
-    fopen("../Views/" . strtoupper($vista)_ "SHOWCURRENT_View.php","w+");
+function crearVistas($tabla){
+    fopen("../Views/" . strtoupper($tabla) . "_ADD_View.php","w+");
+    fopen("../Views/" . strtoupper($tabla) . "SEARCH_View.php","w+");
+    fopen("../Views/" . strtoupper($tabla) . "EDIT_View.php","w+");
+    fopen("../Views/" . strtoupper($tabla) . "DELETE_View.php","w+");
+    fopen("../Views/" . strtoupper($tabla) . "SHOWALL_View.php","w+");
+    fopen("../Views/" . strtoupper($tabla) . "SHOWCURRENT_View.php","w+");
+}
+
+function crearADD($tabla){
+
+}
+
+function crearSEARCH($tabla){
+
+}
+
+
+function crearEDIT($tabla){
+
+}
+
+function crearDELETE($tabla){
+
+}
+
+function crearSHOWALL($tabla){
+
+}
+
+function crearSHOWCURRENT($tabla){
+
+
 }
 ?>
